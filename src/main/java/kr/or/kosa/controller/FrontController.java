2f08e7897cc213a3738f1a99a9527552e20d7327
Bamjong <qjawhd100@naver.com>
2023-01-19 16:17:25 +0900

    프론트컨트롤러 핫픽스!!

---
+++ src/main/java/kr/or/kosa/controller/FrontController.java
@@ -0,0 +1,54 @@
+package kr.or.kosa.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FrontController.class);
+
+	@GetMapping("")
+	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+		return "index";
+	}
+	
+	@GetMapping("/mypage")
+	public String myPage() {
+		
+		return "client/mypage";
+	}
+	
+	@GetMapping("/community")
+	public String community() {
+		
+		return "client/community";
+	}
+	
+	@GetMapping("/mealticket")
+	public String mealticket() {
+		
+		return "client/mealticket";
+	}
+	
+	@GetMapping("/application")
+	public String application() {
+		
+		return "client/application";
+	}
+	
+}
