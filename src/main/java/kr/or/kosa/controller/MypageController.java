d548d9149a26ebe0036ba11b20d099e4fae9eaf6
DOMAYA <c2hila@gmail.com>
2023-01-30 17:23:27 +0900

    기능 주석

--- src/main/java/kr/or/kosa/controller/MypageController.java
+++ src/main/java/kr/or/kosa/controller/MypageController.java
@@ -1,5 +1,23 @@
 package kr.or.kosa.controller;
 
-public class MypageController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/mypage")
+public class MypageController {
+	
+	//비동기를 위한 RestController임
+	
+	//내 정보 조회
+	
+	//내 정보 수정
+	
+	//내 외박 내역 조회
+	
+	//내 벌점 조회
+	
+	//내 커뮤니티 조회
+	
+	//쪽지함 ???
 }
