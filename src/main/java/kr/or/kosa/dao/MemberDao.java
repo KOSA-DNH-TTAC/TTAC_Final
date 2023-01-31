f4c2c4dc6cf1af79c75e196e5fe2222d458667bd
DOMAYA <c2hila@gmail.com>
2023-01-31 17:06:34 +0900

    MemberDao 초안 작성

--- src/main/java/kr/or/kosa/dao/MemberDao.java
+++ src/main/java/kr/or/kosa/dao/MemberDao.java
@@ -1,5 +1,20 @@
 package kr.or.kosa.dao;
 
-public class MemberDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import kr.or.kosa.dto.Member;
+
+public interface MemberDao {
+	
+	//회원 전체 정보(같은 학교 기숙사생만 조회)
+	public List<Member> getAllMember(String universitycode);
+	
+	//특정 회원 조회
+	public Member getMember(String memberid);
+	
+	//정보 수정(마이페이지)
+	
+	
+	//
 }
