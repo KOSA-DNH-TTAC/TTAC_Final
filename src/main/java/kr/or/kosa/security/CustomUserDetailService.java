748261572e6e792be9a10dcbf722a4195c15c7aa
DOMAYA <c2hila@gmail.com>
2023-02-01 13:52:22 +0900

    시큐리티 로그인 UserDetailServcie

--- src/main/java/kr/or/kosa/security/CustomUserDetailService.java
+++ src/main/java/kr/or/kosa/security/CustomUserDetailService.java
@@ -1,16 +1,34 @@
 package kr.or.kosa.security;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class CustomUserDetailService implements UserDetailsService {
+import kr.or.kosa.dao.MemberDao;
+import kr.or.kosa.dto.Member;
+import kr.or.kosa.service.MemberService;
 
+public class CustomUserDetailService implements UserDetailsService {
+	
+	@Autowired
+	MemberDao dao;
+	
 	@Override
 	public UserDetails loadUserByUsername(String memberid) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
 		
-		return null;
+		System.out.println("memberid : " + memberid);
+		
+
+		System.out.println("Load User By Memberid : " + memberid);
+		System.out.println("멤버dao autowired됐나? : " + dao);
+		//userName means userid
+		Member dto = dao.getMember(memberid);
+		System.out.println("queried by memberdao : " + dto);
+		
+		
+		return dto == null ? null : new CustomUser(dto);
 	}
 
 }
