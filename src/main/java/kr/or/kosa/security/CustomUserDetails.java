e48ab2880833fff553a0412bfd56eca789cb5e7d
DOMAYA <c2hila@gmail.com>
2023-02-02 22:42:37 +0900

    시큐리티 userDetails 시리얼 추가

--- src/main/java/kr/or/kosa/security/CustomUserDetails.java
+++ src/main/java/kr/or/kosa/security/CustomUserDetails.java
@@ -3,17 +3,17 @@ package kr.or.kosa.security;
 import java.util.Collection;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 
 import kr.or.kosa.dto.Member;
 import kr.or.kosa.dto.MemberAuth;
-import kr.or.kosa.service.MemberService;
-import lombok.Data;
 
 public class CustomUserDetails implements UserDetails {
+	
+
+	private static final long serialVersionUID = 1L;
+	
 	
 	private String username;
 	private String universityCode;
