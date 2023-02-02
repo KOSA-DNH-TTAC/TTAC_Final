9af1dfdec87383998a8dcdd7e2662553fba3dddd
Yebin1 <yb632339@gmail.com>
2023-02-02 14:27:49 +0900

    BoardDao, BoardMapper 통합
    유사오버로딩

--- src/main/java/kr/or/kosa/dao/BoardDao.java
+++ src/main/java/kr/or/kosa/dao/BoardDao.java
@@ -13,18 +13,6 @@ public interface BoardDao {
 	// 기본 제공 게시판 글 목록
 	public List<Post> allBoardList(String allBoard);
 	
-	// 공지사항
-	public List<Post> noticeList();
-	
-	// 건의사항
-	public List<Post> opinionList();
-	
-	// 자유게시판
-	public List<Post> freeBoardList();
-	
-	// 거래게시판
-	public List<Post> productBoardList();
-	
 	// 커스텀 생성 게시판
 	public List<Post> customBoardList(String boardName);
 
