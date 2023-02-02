52e50f55433a5b2bc5aceacb847871e6c4d32b93
DOMAYA <c2hila@gmail.com>
2023-02-02 22:54:19 +0900

    Chatting누가건드림???

--- src/main/java/kr/or/kosa/controller/MypageController.java
+++ src/main/java/kr/or/kosa/controller/MypageController.java
@@ -5,12 +5,14 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.kosa.dto.Member;
+import kr.or.kosa.security.CustomUserDetails;
 import kr.or.kosa.service.MemberService;
 
 @RestController
@@ -29,11 +31,14 @@ public class MypageController {
 	//내 정보 조회
 	@GetMapping("/myinfo")
 	public ResponseEntity<Member> myinfo(Principal principal){
-		Member member = null;
-		String memberid = principal.getName();
-		member = memberservice.getMemberById(memberid);
+//		Member member = null;
+//		String memberid = principal.getName();
+//		member = memberservice.getMemberById(memberid);
+		CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Member member = memberservice.getMemberById(user.getMemberId());
 		return new ResponseEntity<Member>(member, HttpStatus.OK);
 	}
+	
 	
 	//내 정보 수정
 	
