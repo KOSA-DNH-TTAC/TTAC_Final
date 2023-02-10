71df8550ac4a11eca9b0b37cf024558bd12033c8
JunHans <ii001887@gmail.com>
2023-02-10 22:32:36 +0900

    시설물신고 인서트 완료ㅜㅜㅠ

--- src/main/java/kr/or/kosa/dao/FacilityDao.java
+++ src/main/java/kr/or/kosa/dao/FacilityDao.java
@@ -2,6 +2,8 @@ package kr.or.kosa.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import kr.or.kosa.dto.Facility;
 
 public interface FacilityDao {
@@ -12,9 +14,15 @@ public interface FacilityDao {
 	//시설물 DB 넣기
 	public int insertDomitory(String universitycode, String domitoryname, String domitoryfloor);
 	
-	// 기본 제공 게시판 글 목록
+	//리스트 받아오기
 	public List<Facility> selectItem();
 	
+	//시설물 신고 인서트
+	public int insertReport(@Param("facilityidx")int facilityidx, @Param("domitoryname")String domitoryname, @Param("domitoryfloor")String domitoryfloor,@Param("facilityname")String facilityname,@Param("facilityReport")String facilityReport, @Param("memberid")String memberid);
+	
+	//특정 시설물 조회
+	public Facility selectReportItem(String universityCode, String facilityname);
+
 //	//점호데이터 넣기
 //	public int eveningCallInsert(String memberId, String universitycode);
 //	
