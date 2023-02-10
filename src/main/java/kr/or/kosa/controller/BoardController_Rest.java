ca3a1380ec1e56267214b4660caf28f0dc13fa39
Yebin1 <yb632339@gmail.com>
2023-02-10 18:33:22 +0900

    대댓글 진짜 완료

--- src/main/java/kr/or/kosa/controller/BoardController_Rest.java
+++ src/main/java/kr/or/kosa/controller/BoardController_Rest.java
@@ -37,6 +37,7 @@ public class BoardController_Rest {
 	public void setBoardService(BoardService boardService) {
 		this.boardService = boardService;
 	}
+
 	@Autowired
 	public void setFacilityService(FacilityService facilityService) {
 		this.facilityService = facilityService;
@@ -66,125 +67,75 @@ public class BoardController_Rest {
 			map.put("replyContent", boardService.replyContent(idx));
 			map.put("reReplyContent", boardService.reReplyContent(idx));
 			return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
-			}
-		}
-	
-	/*
-	// 게시판 댓글 보기
-	@GetMapping("/board/{allBoard}/{idx}/reply")
-	public ResponseEntity<List<Reply>> replyContent(@PathVariable("idx") String idx) {
-		List<Reply> replyContent = new ArrayList<Reply>();
-		try {
-			replyContent = boardService.replyContent(idx);
-			return new ResponseEntity<List<Reply>>(replyContent, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<List<Reply>>(replyContent, HttpStatus.BAD_REQUEST);
 		}
 	}
-	
-	
-	// 게시판 대댓글 보기
-	@GetMapping("/board/{allBoard}/{idx}/reply/{replyIdx}")
-	public ResponseEntity<List<Reply>> reReplyContent(@PathVariable("idx") String idx,
-			@PathVariable("replyIdx") String replyIdx) {
-		List<Reply> reReplyContent = new ArrayList<Reply>();
+
+	// 저녁점호 위치비교 + 중복체크 + 데이터 인서트
+	@RequestMapping(value = "/eveningCall", method = RequestMethod.POST, produces = "application/text; charset=utf8")
+	public String eveningCall(@RequestParam(value = "report[]") double[] report) {
+		System.out.println("lat : " + report[0]);
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = user.getMemberId();
+		String unicode = user.getUniversityCode();
+
+		System.out.println("memberid : " + memberId);
+		double lat = report[0];
+		System.out.println("lat : " + lat);
+		double lon = report[1];
+		System.out.println("lon : " + lon);
+		String result = boardService.eveningCall(lat, lon);
+		// boardService.eveningCall(lat, lon);
+
+		// 현재 날짜/시간
+		Date now = new Date();
+		// 포맷팅 정의
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		// 포맷팅 적용
+		String date = formatter.format(now);
+		System.out.println("포멧팅 현재시간 : " + date);
+
+		// 점호한 인원 정보 데이터 인서트
+		String result2 = boardService.eveningCallCompare(memberId, unicode, date);
+		System.out.println("중복 체크 결과 : " + result2);
+		String result3 = result2 + " : 이미 점호 완료한 회원입니다.";
+		if (result2.equals("SUCCESS")) {
+			boardService.eveningCallInsert(memberId, unicode);
+			result3 = result2 + " : 점호가 완료되었습니다.";
+			System.out.println("result3 : " + result3);
+		}
+		System.out.println("result3 : " + result3);
+		return result3;
+	}
+
+	// 시설물 DB 인서트
+	@RequestMapping("/insertItem")
+	public ResponseEntity<List<Facility>> insertItem(@RequestParam String item) {
+		List<Facility> faclist = new ArrayList<Facility>();
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String unicode = user.getUniversityCode();
+		System.out.println("unicode : " + unicode);
+
+		// 들어갔는지 row 수 반환
+		Integer result = facilityService.insertItem(unicode, item);
+		System.out.println("인서트 결과 추가된 ROW : " + result);
 		try {
-			reReplyContent = boardService.reReplyContent(replyIdx);
-			return new ResponseEntity<List<Reply>>(reReplyContent, HttpStatus.OK);
+			faclist = facilityService.selectItem();
+			return new ResponseEntity<List<Facility>>(faclist, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<List<Reply>>(reReplyContent, HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<Facility>>(faclist, HttpStatus.BAD_REQUEST);
 		}
 	}
-	*/
 
+	// 시설물 DB 테이블만 출력
+	@RequestMapping("/itemPrint")
+	public ResponseEntity<List<Facility>> itemPrint() {
+		List<Facility> faclist = new ArrayList<Facility>();
+		try {
+			faclist = facilityService.selectItem();
+			return new ResponseEntity<List<Facility>>(faclist, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<List<Facility>>(faclist, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-		
-		//저녁점호 위치비교 + 중복체크 + 데이터 인서트
-		 @RequestMapping(value = "/eveningCall", method = RequestMethod.POST, produces = "application/text; charset=utf8")
-		   public String eveningCall(@RequestParam(value = "report[]") double[] report) {
-			 System.out.println("lat : "+ report[0]);
-			 User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			 String memberId = user.getMemberId();
-			 String unicode = user.getUniversityCode();
-			
-			 System.out.println("memberid : "+memberId);
-			 double lat = report[0];
-			 System.out.println("lat : "+ lat);
-			 double lon = report[1];
-			 System.out.println("lon : "+ lon);
-			 String result = boardService.eveningCall(lat, lon);
-			 //boardService.eveningCall(lat, lon);
-			 
-			 // 현재 날짜/시간
-			 Date now = new Date();
-		     // 포맷팅 정의
-		     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		     // 포맷팅 적용
-		     String date = formatter.format(now);
-		     System.out.println("포멧팅 현재시간 : "+date);
-		     
-			 //점호한 인원 정보 데이터 인서트
-			 String result2 = boardService.eveningCallCompare(memberId, unicode, date);
-			 System.out.println("중복 체크 결과 : "+result2);
-			 String result3 = result2+" : 이미 점호 완료한 회원입니다.";
-			 if(result2.equals("SUCCESS")) {
-				 boardService.eveningCallInsert(memberId, unicode);
-				 result3 = result2+" : 점호가 완료되었습니다.";
-				 System.out.println("result3 : "+ result3);
-			 }
-			 System.out.println("result3 : "+ result3);
-			 return result3;
-		 }
-		 
-		//시설물 DB 인서트
-			@RequestMapping("/insertItem")
-			public ResponseEntity<List<Facility>> insertItem(@RequestParam String item) {
-				List<Facility> faclist = new ArrayList<Facility>();
-				 User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-				 String unicode = user.getUniversityCode();
-				 System.out.println("unicode : "+unicode);
-				 
-				 //들어갔는지 row 수 반환
-				 Integer result = facilityService.insertItem(unicode, item);
-				 System.out.println("인서트 결과 추가된 ROW : "+result);
-				try {
-					faclist = facilityService.selectItem();
-					return new ResponseEntity<List<Facility>>(faclist, HttpStatus.OK);
-				} catch (Exception e) {
-					return new ResponseEntity<List<Facility>>(faclist, HttpStatus.BAD_REQUEST);
-				}
-			}
-			
-			//시설물 DB 테이블만 출력
-			@RequestMapping("/itemPrint")
-			public ResponseEntity<List<Facility>> itemPrint() {
-				List<Facility> faclist = new ArrayList<Facility>();
-				try {
-					faclist = facilityService.selectItem();
-					return new ResponseEntity<List<Facility>>(faclist, HttpStatus.OK);
-				} catch (Exception e) {
-					return new ResponseEntity<List<Facility>>(faclist, HttpStatus.BAD_REQUEST);
-				}
-			}
-		 
-		//기숙사 건물  DB 인서트
-//		 @RequestMapping(value = "/insertDomitory", method = RequestMethod.POST, produces = "application/text; charset=utf8")
-//		   public String insertDomitory(@RequestParam(value = "domitory[]") String[] domitory) {
-//			 System.out.println("domitory : "+ domitory);
-//			 User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//			 String unicode = user.getUniversityCode();
-//			 System.out.println("unicode : "+unicode);
-//			 
-//			 String domitoryname = domitory[0];
-//			 String domitoryfloor = domitory[1];
-//			 
-//			 //들어갔는지 row 수 반환
-//			 Integer result = facilityService.insertDomitory(unicode, domitoryname, domitoryfloor);
-//			 System.out.println("인서트 결과 추가된 ROW : "+result);
-//			 String result1 = "기숙사 건물 입력성공";
-//			 
-//			 return result1;
-//		 }
-		 
-		
 }
