353f53b20254bcf050e2aba17fdbe04c9a0ec5fd
Yebin1 <yb632339@gmail.com>
2023-02-14 11:48:40 +0900

    특정 기숙사생 정보 조회 완료 +벌점 하는중..

--- src/main/java/kr/or/kosa/dao/AdminDao.java
+++ src/main/java/kr/or/kosa/dao/AdminDao.java
@@ -1,10 +1,19 @@
 package kr.or.kosa.dao;
 
+import java.util.List;
+
+import kr.or.kosa.dto.DemeritHistory;
+import kr.or.kosa.dto.Member;
+
 public interface AdminDao {
 	
 	//기숙사생 전체 조회
 	
 	//기숙사생 특정 조회
+	public List<Member> getMemberInfo(String memberId, String universitycode);
+	
+	// 기숙사생 벌점
+	public void setDemerit(DemeritHistory demerit);
 	
 	//처리되지 않은 외박 신청 조회
 	
