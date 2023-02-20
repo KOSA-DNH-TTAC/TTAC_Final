c48837c962c506385b74e5b3325cd40a074603ed
DOMAYA <c2hila@gmail.com>
2023-02-20 13:28:35 +0900

    웹소켓 configuer cross origin

--- src/main/java/kr/or/kosa/chat/WebSocketConfig.java
+++ src/main/java/kr/or/kosa/chat/WebSocketConfig.java
@@ -1,23 +1,23 @@
 package kr.or.kosa.chat;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import lombok.RequiredArgsConstructor;
+	
+	@Configuration
+	@RequiredArgsConstructor
+	@EnableWebSocket
+	@CrossOrigin(origins = "*")
+	public class WebSocketConfig implements WebSocketConfigurer{
 
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer{
+	    private final ChattingHandler chatHandler;
 
-	  private final ChattingHandler chatHandler;
-	  
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		// TODO Auto-generated method stub
-		 registry.addHandler(chatHandler, "chat").setAllowedOrigins("*");
+	    @Override
+	    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	        registry.addHandler(chatHandler, "chat").setAllowedOrigins("*");
+	    }
 	}
-
-}
