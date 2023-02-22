8cd092574cbdfc23dcadbb49db91029bb53db689
JunHans <ii001887@gmail.com>
2023-02-22 22:25:58 +0900

    점호2

--- src/main/java/kr/or/kosa/controller/AdminController_Rest.java
+++ src/main/java/kr/or/kosa/controller/AdminController_Rest.java
@@ -278,9 +278,11 @@ public class AdminController_Rest {
 	// 관리자 점호 회원  조회
 		@RequestMapping("/admin/allRollCallMember")
 		public ResponseEntity<List<RollCall>> getAllRollCallMember(@RequestParam String date) {
+			System.out.println("점호 컨트롤러");
 			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 			String universitycode = user.getUniversityCode();
 			String domitoryName = user.getDomitoryName();
+			System.out.println("wjaghzjsxmfhffj 2");
 			List<RollCall> list = adminService.getAllRollCallMember(universitycode,domitoryName,date);
 			try {
 				return new ResponseEntity<List<RollCall>>(list, HttpStatus.OK);
