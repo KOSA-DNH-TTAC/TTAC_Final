05563c2accbecf70524a275093d19f94ad38dc2a
Yebin1 <yb632339@gmail.com>
2023-02-24 16:05:28 +0900

    전체 syso 삭제 완료

--- src/main/java/kr/or/kosa/aspect/LoginAspect.java
+++ src/main/java/kr/or/kosa/aspect/LoginAspect.java
@@ -18,79 +18,23 @@ import lombok.extern.log4j.Log4j;
 @Component
 @Log4j
 public class LoginAspect {
-	
-	private static final Log LOG = LogFactory.getLog( LoginAspect.class );
-	
-//    @Around("execution(* kr.or.kosa.security.CustomUserDetailService.loadUserByUsername(..))")
-//    @Around("execution(* kr.or.kosa.security..*.*(..))")
-//	@Pointcut("execution(* kr.or.kosa.security..*.*(..))")
-//	@Pointcut("execution(* kr.or.kosa..*(..))")
-//	@Pointcut("execution(* kr.or.kosa.security..*.*(..))")
-//	 @Pointcut("execution(* kr.or.kosa.security.CustomUserDetailService.loadUserByUsername(String))")
+
+	private static final Log LOG = LogFactory.getLog(LoginAspect.class);
+
 	@Pointcut("execution(* kr.or.kosa.utils.VisitorInfo.*(..))")
-	public void logMe() {}
-    
+	public void logMe() {
+	}
+
 	@AfterReturning(value = "logMe()", returning = "returnValue")
-    public void aroundLoadUserByUsername(JoinPoint joinPoint, Object returnValue) throws Throwable {
-//		LOG.debug( "#ex1 - debug log" );
-//		LOG.info( "#ex1 - info log" );
-//		LOG.warn( "#ex1 - warn log" );
-//		LOG.error( "#ex1 - error log" );
-		
+	public void aroundLoadUserByUsername(JoinPoint joinPoint, Object returnValue) throws Throwable {
+
 		LOG.info("====== AOP : 로그인 ======");
 		LOG.info(returnValue);
-		
-		//콘솔출력용
-    	log.info("==================================== AOP : 로그인 ======================================");
+
+		// 콘솔출력용
+		log.info("==================================== AOP : 로그인 ======================================");
 		log.info(returnValue);
 		log.info("=========================================================================================");
-    }
-
-	/*
-	  public Object aroundLoadUserByUsername(ProceedingJoinPoint joinPoint) throws Throwable {
-
-//	        log.info("Called method : {}", joinPoint.getSignature().getName());
-	        log.info("???");
-	        log.info("파라미터 : " + joinPoint.getArgs());
-//	        log.info("With arguments : {}", joinPoint.getArgs());
-
-	        System.out.println("제발 ㅠㅠ");
-	        Object result = joinPoint.proceed();
-
-	        log.info("리턴 : " + result);
-//	        log.info("Returned value : {}", result);
-
-	        return result;
-	    }
-	    */
-/*	
-	@Pointcut("execution(* kr.or.kosa.security.CustomUserDetailService.loadUserByUsername(..))")
-	public void advicePoint() {}
-
-	@Around("advicePoint()")
-	public Object aroundLoadUserByUsername(ProceedingJoinPoint pjp) {
-		System.out.println("제발 !!!");
-		System.out.println("pjp...?" + pjp.getSignature());
-		log.info("==================Around==============");
-		log.info("로그인 시도에 이용된 유저 정보 : " + pjp.getArgs());
-		
-		Object result = null;
-		
-		try {
-			result = pjp.proceed();
-		}catch (Throwable e) {
-			log.info("에러! " + e.getMessage());
-			e.printStackTrace();
-		}
-		return result;
 	}
-	
-	//리턴 객체가 있는 경우
-	@AfterReturning(value = "advicePoint()", returning = "returnValue")
-	public void logAfter(JoinPoint joinPoint, Object returnValue) {
-		System.out.println("ㅈ발 !!");
-		 log.info("=============AfterReturning================");
-		 log.info("리턴값 : " + returnValue);
-	}
-	*/
+
 }
